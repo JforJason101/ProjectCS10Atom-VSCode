e7ee9515378a1d79f65bae88101bdd1cc82b51ec
Jaskaran Singh <j.singh70@EDU.epsb.ca>
2020-11-19 15:12:52 -0700

    Update design.js

--- MyProjects/Proj1/design.js
+++ MyProjects/Proj1/design.js
@@ -1,1 +1,99 @@
-const
+
+function alerttt() {
+
+  // Event Listener
+  document.getElementById("btn").addEventListener("click", greeting);
+
+  // Link
+
+  const link = document.createElement('Link');
+  var linkText = document.createTextNode("link");
+  link.appendChild(linkText);
+  link.title = "link";
+  link.href = "https://www.youtube.com/watch?v=DL zxrzFCyOs";
+  document.body.appendChild(link);
+
+  // Input
+
+  var name = prompt("What's your name?");
+
+  // Display
+
+  const message = 'Hello ' + name +  ' Please press on this ' + link;
+
+  // Output
+  document.getElementById("alerttt").innerHTML = message;
+  alert(message);
+}
+
+
+/* We can then select these HTML IDs (in the quotations)
+and store references to them in variables like so */
+const quizContain = document.getElementById('quizhold');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+/* We can then select these HTML IDs (in the quotations)
+and store references to them in variables like so */
+
+function theQuiz() {
+    // Var. to store the quiz of course
+  const Output[];
+}
+
+function displayResults() {
+
+}
+
+theQuiz();
+
+submitButton.addEventListener('click')
+
+const theQs = [
+  {
+    question: "Who is Karl Marx?"
+    answers: {
+      a: "Godfather of Capitalism",
+      b: "Santa Claus's father",
+      c: "Father of Adam Smith",
+      d: "None"
+    },
+    ans: "d"
+  },
+
+  {
+    question: italics("TASK:")"Say 'hi' to juan."
+    const img = document.createElement('img')
+    img.src = 'https://i.kym-cdn.com/entries/icons/mobile/000/035/644/juancover.jpg'
+    answers: {
+      a: "y e s",
+      b: "N O"
+    },
+      ans: "b"
+  },
+
+  {
+    question: "What is socialism?""
+    answers: {
+      a: "When the gubberment does stuff!",
+      b: "When capitalism",
+      c: "when workers own business",
+      d: "when no iphone"
+    },
+    ans: "c"
+  },
+
+  {
+    question: "What is socialism?""
+    answers: {
+      a: "When the gubberment does stuff!",
+      b: "When capitalism",
+      c: "when workers own business",
+      d: "when no iphone"
+    },
+    ans: "c"
+  }
+
+      }
+
+    }
+];
