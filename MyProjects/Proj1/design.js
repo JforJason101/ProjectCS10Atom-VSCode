942e180a6a9acf47a1091357afe5a06a64f12a26
Jaskaran Singh <j.singh70@EDU.epsb.ca>
2020-11-20 15:29:28 -0700

    Changes

--- MyProjects/Proj1/design.js
+++ MyProjects/Proj1/design.js
@@ -1,54 +1,105 @@
+// FUNCTIONS -->
 
-// Event Listener
-document.getElementById("btn").addEventListener("click", greeting);
+function theQuiz() {
 
-function alerttt() {
+// to store the quiz of course
+const output = [];
 
-  // Input
-  let thing - document.getElementById('')
-  let name = prompt("What's your name?");
+// for each of my questions
+theQs.forEach( (currentQ, QNumber) => {
+    // code to run each question goes here
+
+      // stores answers
+      let answer = [];
+
+      // for each available answer (...)
+      for(letter in currentQ.answer) {
+
+        // HTML radio button
+        answer.push(
+          `<label>
+          <input type= "radio" name="question${QNumber}" value = "${letter}">
+          ${letter} :
+          ${currentQ.answer[letter]}
+          </label>`
+        );
+      }
+
+      // add question and the answers to output
+      output.push(
+        `<div class = "question"> ${currentQ.question} </div>
+         <div class = "answer"> ${answer.join('')} </div>`
+      );
+    }
+  );
+  // combine output list into html string and print it
+  quizContainer.innerHTML = output.join('');
+}
+
+function displayResults() {
+
+  // grab the answer container
+  const answerContainers = quizContainer.querySelectorAll('.answer');
+
+  // user's answers
+  let numCorrect = 0;
+
+  // for each question...
+  myQs.forEach( (currentQ,QNumber) => {
+
+    // find selected answer
+    const answerContainers2 =  answerContainers[questionNumber];
+    const selector = `input[name=question${questionNumber}]:checked`;
+    const userAnswer = (answerContainer.querySelector(Selector) || {}).value;
+
+    // 'if the answer is correct' statement
+    if(userAnswer === currentQ.correctAnswer) {
+      // adds to the number of correct answers
+      numCorrect++;
+
+      // colour the answers in the correct color - Green
+      answerContainers2[QNumber].style.colour = 'lightgreen';
+    }
+    // if the answer is blank or incorrect do this -->
+    else {
+      // colour it red
+      answerContainers[questionNumber].style.colour = 'red'
+    }
+  }
+);
+
+  // show the number of correct answers out of total
+  if(numCorrect === myQs.length) {
+    resultsContainer.innerHTML = `Amazin! You got no questions wrong!`
+  }
+  else {
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
+}
 
   // Link
 
   const link = document.createElement('Link');
   let linkText = document.createTextNode("link");
-  link.appendChild(linkText);
-  link.title = "link";
-  link.href = "https://www.youtube.com/watch?v=DL zxrzFCyOs";
-  document.body.appendChild(link);
+    link.appendChild(linkText);
+    link.title = "link";
+    link.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    document.body.appendChild(link);
 
-  // Display
+  // Link
 
-  const message = 'Hello ' + name + '! Please press on this ' + link;
-
-  // Output
-
-  document.getElementById("alerttt").innerHTML = message;
-  alert(message);
-}
+// Variables
 
 /* We can then select these HTML IDs (in the quotations)
-and store references to them in letiables like so */
+and store references to them in varconst theQs variables like so */
+//////////////////////////////////////////////////////////
 const quizContain = document.getElementById('quizhold');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
-/* We can then select these HTML IDs (in the quotations)
-and store references to them in letiables like so */
-
-function theQuiz() {
-  // let. to store the quiz of course
-  const Output[];
-}
-
-function displayResults() {
-
-}
-
-theQuiz();
-
-submitButton.addEventListener('click')
-
-const theQs = [{
+//////////////////////////////////////////////////////////
+const launchlast = document.getElementById('lastquestion').value;
+const theQs = [
+{
   question: "Who is Karl Marx?"
   answers: {
     a: "Godfather of Capitalism",
@@ -65,24 +116,36 @@ const theQs = [{
   const img = document.createElement('img')
   img.src = 'https://i.kym-cdn.com/entries/icons/mobile/000/035/644/juancover.jpg'
   answers: {
-    a: "y e s",
-    b: "N O"
+    a: italics("y e s"),
+    b: italics("N O")
   },
   ans: "b"
 },
 
 {
   question: "What is socialism?"
-  "
   answers: {
-    a: "When the gubberment does stuff!",
+    a: "When the gubberment does stuff",
     b: "When capitalism",
     c: "when workers own business",
     d: "when no iphone"
   },
   ans: "c"
 }
+];
+
+// Variables
+
+
+    // builds quiz
+    theQuiz();
+    // builds quiz
+
+// Event Listeners --->
+submitButton.addEventListener('click')
+
+
+
 }
 
 }
-];
