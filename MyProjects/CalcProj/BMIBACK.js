150e09f23a4190452a5d53a7bea036eb10a55126
JforJason101 <JforJason101@users.noreply.github.com>
2020-11-24 22:21:10 -0700

    REEEEEE!!!

--- MyProjects/CalcProj/BMIBACK.js
+++ MyProjects/CalcProj/BMIBACK.js
@@ -8,35 +8,35 @@ document.getElementById("submitInputValues").addEventListener("click", whenSubmi
 // scope
 
 function whenSubmit() {
-    // HEIGHT VARS. (START)
+	// HEIGHT VARS. (START)
 
-    let heightInput = parseInt(document.getElementById("heightInput").value);
-    // height in CM
+	let heightInput = parseInt(document.getElementById("heightInput").value);
+	// height in CM
 
-    let cmToM = heightInput / 100
-        // changes CM to M
+	let cmToM = heightInput / 100;
+	// changes CM to M
 
-    let theHeightM = cmToM * cmToM;
-    // squares the converted 'M' height
+	let theHeightM = cmToM * cmToM;
+	// squares the converted 'M' height
 
-    // HEIGHT VARS. (END)
+	// HEIGHT VARS. (END)
 
-    // MASS VARS. (START)
-    let massKilos = parseInt(document.getElementById("weightInput").value);
-    // MASS VARS. (END)
+	// MASS VARS. (START)
+	let massKilos = parseInt(document.getElementById("weightInput").value);
+	// MASS VARS. (END)
 
-    //////////////////////////////////////////
+	//////////////////////////////////////////
 
-    let theBMI = massKilos / theHeightM;
-    let rounded = parseFloat(theBMI).toFixed(2)
-    if (rounded === NaN) {
-        console.log("1")
-        document.getElementById("output").innerHTML =
-            "The value inputted cannot be registered, retry";
-    } else {
-        console.log("2")
-        document.getElementById("output").innerHTML = `Your BMI is ${rounded}`;
-        heightInput = '';
-        weightInput = '';
-    }
+	let theBMI = massKilos / theHeightM;
+	let rounded = parseFloat(theBMI).toFixed(2);
+	if (rounded === NaN) {
+		console.log("1");
+		document.getElementById("output").innerHTML =
+			"The value inputted cannot be registered, retry";
+	} else {
+		console.log("2");
+		document.getElementById("output").innerHTML = `Your BMI is ${rounded}`;
+		heightInput = "";
+		weightInput = "";
+	}
 }
