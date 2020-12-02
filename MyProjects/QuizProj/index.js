c5d2fe65c12db82aace377dc974a1578d6e21c70
JforJason101 <JforJason101@users.noreply.github.com>
2020-12-01 18:20:48 -0700

    init

--- MyProjects/QuizProj/index.js
+++ MyProjects/QuizProj/index.js
@@ -14,42 +14,54 @@ function theQs() {
 	// Array consisting of the answers
 	const ans = {
 		q1: "FATHER OF CAPITALISM",
+		qq1: "SANTA CLAUS",
 		q2: "HI",
+		qq2: "HOW ABOUT NO",
+		qqq2: "HOW ABOUT NO!",
 		q3: "WHEN THE GUBBERMENT DOES STUFF",
-		q4: "THEIR CHAINS",
+		qq3: "WHEN NO IPHONE",
+		q4: "NOTHING BUT THEIR CHAINS",
+		qq4: "THEIR CHAINS",
 	};
 
+	// Will display the answer if wrong
 	const ifWrong1 = document.getElementById("ifWrong1");
 	const ifWrong2 = document.getElementById("ifWrong2");
 	const ifWrong3 = document.getElementById("ifWrong3");
 	const ifWrong4 = document.getElementById("ifWrong4");
 
-	// If statements used to check if the inputs match, if they do, it adds 1 to the score.
-	if (q1.value.toUpperCase() === ans.q1) {
+	// If statements used to check if the inputs match. If they do, it adds 1 to the score.
+	if (q1.value.toUpperCase() === ans.q1 || q1.value.toUpperCase() === ans.qq1) {
 		score += 1;
 	} else {
-		ifWrong1.innerHTML = `WRONG! The answer for Number 1, was: 'Father of Capitalism'`;
+		ifWrong1.innerHTML = `WRONG! The answer for Number 1, was: 'Father of Capitalism' or 'Santa Claus'`;
 	}
-	if (q2.value.toUpperCase() === ans.q2) {
+	if (
+		q2.value.toUpperCase() === ans.q2 ||
+		q2.value.toUpperCase() === ans.qq2 ||
+		q2.value.toUpperCase() === ans.qqq2
+	) {
 		score += 1;
 	} else {
-		ifWrong2.innerHTML = `WRONG! The answer for Number 2, was: 'hi'`;
+		ifWrong2.innerHTML = `WRONG! The answer for Number 2, was: 'hi' or 'how about no'`;
 	}
-	if (q3.value.toUpperCase() === ans.q3) {
+	if (q3.value.toUpperCase() === ans.q3 || q3.value.toUpperCase() === ans.qq3) {
 		score += 1;
 	} else {
-		ifWrong3.innerHTML = `WRONG! The answer for Number 3, was: 'When the Gubberment does stuff'`;
+		ifWrong3.innerHTML = `WRONG! The answer for Number 3, was: 'When the Gubberment does stuff' or 'When no iPhone'`;
 	}
-	if (q4.value.toUpperCase() === ans.q4) {
+	if (q4.value.toUpperCase() === ans.q4 || q4.value.toUpperCase() === ans.qq4) {
 		score += 1;
 	} else {
-		ifWrong4.innerHTML = `WRONG! The answer for Number 4, was: 'Their chains'`;
+		ifWrong4.innerHTML = `WRONG! The answer for Number 4, was: 'Nothing but their chains' or 'Their chains'`;
 	}
 	console.log(score);
 
 	// Output Display
 	const mark = document.getElementById("mark");
 	// Display Mark -->
+	// This makes the score into a percentage
+	const percent = (score / 4) * 100;
 
-	mark.innerHTML = `You receive a ${score}/4!`;
+	mark.innerHTML = `Well done! You receive a ${score}/4! (${percent}%)`;
 }
