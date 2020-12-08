b49adbce4a7d4b9386625e5a71a410b32f062a83
JforJason101 <JforJason101@users.noreply.github.com>
2020-12-08 14:59:40 -0700

    init

---
+++ MyProjects/CalcAverageCodeAlong/script.js
@@ -0,0 +1,13 @@
+// Input
+let x = prompt("Enter your first number!");
+let num1 = parseInt(x);
+let y = prompt("Enter your second number!");
+let num2 = parseInt(y);
+let z = prompt("Enter your third number!");
+let num3 = parseInt(z);
+
+// Process
+let average = (num1 + num2 + num3) / 3;
+
+// Output
+alert(`Your average is: ${average}`);
