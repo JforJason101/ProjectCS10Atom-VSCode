ac4edc17238f4f4ab628de1067efae5e39addad4
Jaskaran Singh <j.singh70@EDU.epsb.ca>
2021-01-12 15:29:51 -0700

    com

--- MyProjects/FinalProj/tailwind.config.js
+++ MyProjects/FinalProj/tailwind.config.js
@@ -1,28 +1,10 @@
-const plugin = require('tailwindcss/plugin')
+const plugin = require("tailwindcss/plugin");
 
 module.exports = {
 	purge: [],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {
-			textColor: [
-        	'responsive',
-        	'hover',
-        	'focus',
-        	'before',
-        	'after',
-        	// If you want to combine it with a pseudo class,
-        	// use `<pseudo-class>_<pseudo-element>`.
-        	'hover_before',
-        	'hover_after',
-        	'focus_before',
-			'foo_bar',
-			],
-		},
-		backgroundColor: (theme) => ({
-			coolGREEN: "#c3ff00",
-			black: "#000000"
-		}),
+		extend: {},
 	},
 	variants: {
 		extend: {},
