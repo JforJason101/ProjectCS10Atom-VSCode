606d5f7751e3fa75b8c1625ec9868b5e6afd6040
JforJason101 <jasonkauara@gmail.com>
2021-01-22 15:26:48 -0700

    com

--- MyProjects/FinalProj/dist/js/script.js
+++ MyProjects/FinalProj/dist/js/script.js
@@ -7,7 +7,7 @@ let countingPart = 0;
 let pI = 0;
 // KEEPS TRACK OF INDIVIDUAL LETTERS (using 'ints') IN A STRING (INDEX)
 
-let intervalVal;
+var intervalVal;
 // Holds the reference that setInterval creates
 
 let element = document.querySelector(".typing");
@@ -30,33 +30,6 @@ function type() {
 		}, 1000);
 	}
 }
-
-// function del() {
-// 	// Get substring, deletes a character
-// 	var text = texts[countingPart].substring(0, pI - 1);
-// 	element.innerHTML = text;
-// 	pI--;
-
-// 	// Moves to the next sentence
-// 	if (text === "") {
-// 		clearInterval(intervalVal);
-
-// 		/* If the current sentence was the last one in the
-//         array then display [0] in the array,
-//         ELSE, move to the next one. */
-
-// 		if (countingPart == texts.length - 1) countingPart = 0;
-// 		else countingPart++;
-
-// 		pI = 0;
-
-// 		// Delay next sentence
-
-// 		setTimeout(function () {
-// 			intervalVal = setInterval(type, 100);
-// 		}, 200);
-// 	}
-// }
 
 // Start the typing effect on load
 intervalVal = setInterval(type, 100);
